7d3c6730e46956f96f751773b4d71466b0c63a06
mdipaolo24 <mitchell.dipaolo@fiserv.com>
2023-11-06 14:59:05 -0500

    Hey Dhruv ;)

--- ECSource.java
+++ ECSource.java
@@ -1,24 +1,74 @@
-class Node<T> {
-  T val;
-  Node<T> next;
-
-  public Node(T val) {
-    this.val = val;
-    this.next = null;
-  }
-}
+//class Node<T> {
+//  T val;
+//  Node<T> next;
+//
+//  public Node(T val) {
+//    this.val = val;
+//    this.next = null;
+//  }
+//}
 
 class ECSource {
   public static <T> boolean isUnivalueList(Node<T> head) {
-    // todo
-    return true;
+    // If the list is empty, return true as there are no elements to compare.
+    if (head == null) {
+      return true;
+    }
+
+    // Get the value of the first node to compare with the rest of the list.
+    T valueToCompare = head.val;
+
+    // Recursive check for the rest of the list.
+    return isUnivalueListRecursive(head.next, valueToCompare);
+  }
+
+  private static <T> boolean isUnivalueListRecursive(Node<T> current, T valueToCompare) {
+    // Base case: if we reach the end of the list, return true.
+    if (current == null) {
+      return true;
+    }
+
+    // Check if the current node's value is equal to the value we want to compare.
+    if (!current.val.equals(valueToCompare)) {
+      return false;
+    }
+
+    // Continue checking the rest of the list recursively.
+    return isUnivalueListRecursive(current.next, valueToCompare);
   }
 
   public static void main(String[] args) {
-        Node<String> z = new Node<>("z");
-        // z
+    Node<Integer> u = new Node<>(2);
+    Node<Integer> v = new Node<>(2);
+    Node<Integer> w = new Node<>(2);
+    Node<Integer> x = new Node<>(2);
+    Node<Integer> y = new Node<>(2);
 
-        // Printing solution
-        System.out.println((ECSource.isUnivalueList(z)));
-    }
+    u.next = v;
+    v.next = w;
+    w.next = x;
+    x.next = y;
+
+    // Print the result for the linked list u
+    System.out.println(ECSource.isUnivalueList(u)); // Should print "true"
+
+    Node<Integer> u2 = new Node<>(2);
+    Node<Integer> v2 = new Node<>(2);
+    Node<Integer> w2 = new Node<>(3);
+    Node<Integer> x2 = new Node<>(3);
+    Node<Integer> y2 = new Node<>(2);
+
+    u2.next = v2;
+    v2.next = w2;
+    w2.next = x2;
+    x2.next = y2;
+
+    // Print the result for the linked list u2
+    System.out.println(ECSource.isUnivalueList(u2)); // Should print "false"
+
+    Node<String> z = new Node<>("z");
+
+    // Print the result for the linked list z
+    System.out.println(ECSource.isUnivalueList(z)); // Should print "true"
+  }
 }
--- Source.java
+++ Source.java
@@ -1,33 +1,67 @@
 class Node<T> {
     T val;
     Node<T> next;
-    
+
     public Node(T val) {
-      this.val = val;
-      this.next = null;
+        this.val = val;
+        this.next = null;
     }
-  }
-  
-  class Source {
+}
+
+class Source {
     public static <T> Node<T> reverseList(Node<T> head) {
-      // todo
-      return head;
+        Node<T> prev = null;
+        Node<T> current = head;
+        Node<T> next = null;
+
+        while (current != null) {
+            next = current.next; // Store the next node
+            current.next = prev; // Reverse the link
+
+            // Move to the next pair of nodes
+            prev = current;
+            current = next;
+        }
+
+        // Update the new head of the reversed list
+        head = prev;
+
+        return head;
     }
-    
+
     public static void main(String[] args) {
         Node<String> x = new Node<>("x");
         Node<String> y = new Node<>("y");
-    
+        Node<String> a = new Node<>("a");
+        Node<String> b = new Node<>("b");
+        Node<String> c = new Node<>("c");
+        Node<String> d = new Node<>("d");
+        Node<String> e = new Node<>("e");
+        Node<String> f = new Node<>("f");
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+
+
+        Node<String> head2 = reverseList(a); // f -> e -> d -> c -> b -> a
+
+
         x.next = y; // x -> y
-    
-        reverseList(x); // y -> x
 
-        // Printing solution
-        Node<String> head = Source.reverseList(x);
+        // Reverse the list and get the new head
+        Node<String> head = reverseList(x); // y -> x
 
+        // Printing the reversed list
         while (head != null) {
             System.out.println(head.val);
-            head = head.next;            
+            head = head.next;
+        }
+        while (head2 != null) {
+            System.out.print(head2.val);
+            head2 = head2.next;
         }
     }
-  }
+}
